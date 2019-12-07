57d8d1e32f71013f249bb93379d0eba0308500f1
celebrated-integer-2023 <scailles23@gmail.com>
2019-12-07 06:49:58 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,17 @@
 
 
 def hello_t(array)
+  if block_given?
   i = 0
  
   while i < array.length
     yield(array[i])
     i = i + 1
- while array.length==0
-   puts "Hey! No block was given!
+ end
+ array
+ 
+ else puts
+   "Hey! No block was given!"
+ end
  end
 
